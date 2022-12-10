146aadf1d95d54f57f7f3d22fdf77aa80efc4940
JBUkim <91812103@jmail.ac.kr>
2022-12-10 15:46:54 +0900

    key2

--- pages/project.js
+++ pages/project.js
@@ -19,7 +19,7 @@ function Portfolio() {
             <p className="description">사진을 클릭하면 사이트로 이동합니다.</p>
 
             <section>
-              {/* // eslint-disable-next-line react/jsx-key */}
+              {/* eslint-disable-next-line react/jsx-key */}
               <div class="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8" key="img">
                 <ul class="grid grid-cols-1 gap-4 mt-8 lg:grid-cols-3">
                   <li>
