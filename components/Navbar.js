f3f961e1d834c90b5d48e2a9f467f730c299b2ac
JBUkim <91812103@jmail.ac.kr>
2022-12-10 16:12:38 +0900

    navbar1

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,15 @@
 import Link from "next/link";
 import { withRouter } from "next/router";
 
-function Navbar({ router }) {
-  const navs = [
-    { text: "Home", href: "/" },
-    { text: "Skills", href: "/skills" },
-    { text: "Project", href: "/project" },
-    { text: "Contact", href: "/contact" },
-    { text: "GitHub", href: "https://github.com/JBUkim" },
-    // { text: '', href: '' },
-  ];
+function Navbar() {
+  // const navs = [
+  //   { text: "Home", href: "/" },
+  //   { text: "Skills", href: "/skills" },
+  //   { text: "Project", href: "/project" },
+  //   { text: "Contact", href: "/contact" },
+  //   { text: "GitHub", href: "https://github.com/JBUkim" },
+  //   // { text: '', href: '' },
+  // ];
 
   return (
     <nav className="navbar">
@@ -39,13 +39,13 @@ function Navbar({ router }) {
             </a>
           </li>
           <li className="relative inline-block mx-5 active:font-bold hover:font-bold">
-            <a href="/">Skills</a>
+            <a href="/skills">Skills</a>
           </li>
           <li className="relative inline-block mx-5 active:font-bold hover:font-bold">
-            <a href="/">Project</a>
+            <a href="/project">Project</a>
           </li>
           <li className="relative inline-block mx-5 active:font-bold hover:font-bold">
-            <a href="/">Contact</a>
+            <a href="/contact">Contact</a>
           </li>
           <li className="relative inline-block mx-5 active:font-bold hover:font-bold">
             <a href="https://github.com/JBUkim">GitHub</a>
